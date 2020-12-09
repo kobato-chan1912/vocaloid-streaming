6e05d51c043a664ec4ce620b84ca0418a43f50cc
MAI VIẾT DŨNG <no_body156@yahoo.com.vn>
2020-12-09 09:10:02 +0700

    fix css and add add ffmpeg

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\future_artists;
 use Illuminate\Http\Request;
-
+use FFMpeg;
 class HomeController extends Controller
 {
     //
@@ -12,5 +12,21 @@ class HomeController extends Controller
         $future = new future_artists();
         $artists = $future->GetArtists();
         return view('homepage', ["artists" => $artists]);
+    }
+    //
+    public function test(){
+        $ffmpeg = FFMpeg\FFMpeg::create(array(
+            'ffmpeg.binaries'  => '/usr/local/bin/ffmpeg',
+            'ffprobe.binaries' => '/usr/local/bin/ffprobe',
+            'timeout'          => 3600, // The timeout for the underlying process
+            'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
+        ));
+        $video = $ffmpeg->open('https://anime47.imfast.io/%5BVnsharing%5DS-G.mp4');
+        var_dump($video->getFormat());
+        echo gmdate("i:s", 258.020000);
+
+        //        $frame = $video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(42));
+//        $frame->save("img/screen.jpg");
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@Homepage');
 
+Route::get('test', 'HomeController@test');
