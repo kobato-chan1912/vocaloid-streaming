7f8a0a0b459034b45c4a1bc0f938f2bd8cc8788f
MAI VIẾT DŨNG <no_body156@yahoo.com.vn>
2020-12-14 09:20:32 +0700

    add videos data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,13 +24,13 @@ class HomeController extends Controller
             'timeout'          => 3600, // The timeout for the underlying process
             'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
         ));
-        $video = $ffmpeg->open('https://anime47.imfast.io/%5BDoremi-FS%5D%20glow%20-%20Keeno%20%28Magical%20Mirai%202015%29.mp4');
+        $video = $ffmpeg->open('https://anime47.imfast.io/Orangestar%20-%20%E3%82%B7%E3%83%B3%E3%82%AF%E3%83%AD%E3%83%8A%E3%82%A4%E3%82%B5%E3%82%99%E3%83%BC%20(feat.%20%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF)%20Official%20Video.mp4');
         var_dump($video->getFormat());
         echo gmdate("i:s", 258.020000);
 
         $frame = $video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(06));
-        mkdir("img/screens/02", 0700);
-        $frame->save("img/screens/02/02_screen.jpg");
+        mkdir("img/screens/04", 0700);
+        $frame->save("img/screens/04/04_screen.jpg");
 
     }
     public function GetCategories($id){
